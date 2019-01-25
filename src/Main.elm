df9aaf18a5916188f354ffff7f162a9833b451c1
Jesse Thompson <jesset@gmail.com>
2019-01-25 05:54:38 -0800

    q

--- src/Main.elm
+++ src/Main.elm
@@ -24,28 +24,62 @@ main =
 
 -- MODEL
 
-type alias Model = List ( List Int )
+type Offset = Offset Int Int
+type Cell
+    =   EmptyCell
+    |   StaticCell Int
+    |   DynamicCell
+        {   merged : Int
+        ,   incumbent : Int
+        ,   moving : Int
+        ,   from : Offset
+        }
+type alias Model = List ( List Cell )
 
 
 -- INIT
 
+
+sprinkle : Int -> Cmd Msg
+sprinkle emptyCells =
+    Random.generate
+        Sprinkle
+        <|  Random.map2
+                (\emptyIndex value ->
+                    {   emptyIndex = emptyIndex
+                    ,   value = value
+                    }
+                )
+                ( Random.int 0 15 )
+                (   Random.weighted
+                        (   90, 2 )
+                        [   (   10, 4 ) ]
+                )
+
+
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( List.repeat
-        4
-        <|List.repeat 4 0
-    , Cmd.none )
+    (   List.repeat
+            4
+            <|  List.repeat 4 EmptyCell
+    ,   Cmd.batch [ sprinkle 16, sprinkle 15 ]
+    )
 
 
 -- UPDATE
 
 
 type Msg
-    = Reset
-    -- | Up
-    -- | Down
-    -- | Left
-    -- | Right
+    =   Reset
+    |   Sprinkle
+        {   emptyIndex : Int
+        ,   value : Int
+        }
+    
+    -- |   Up
+    -- |   Down
+    -- |   Left
+    -- |   Right
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -53,6 +87,21 @@ update msg model =
     case msg of
         Reset ->
             init ()
+
+        Sprinkle { emptyIndex, value } ->
+            (   model
+                |>  List.indexedMap
+                    (\rowIndex row ->
+                        row
+                        |>  List.indexedMap
+                            (\columnIndex cell ->
+                                if x == rowIndex && y == columnIndex
+                                    then StaticCell value
+                                    else cell
+                            )
+                    )
+            ,   Cmd.none
+            )
         
 
 -- SUBSCRIPTIONS
@@ -72,13 +121,23 @@ view model =
             row
             |>  List.map
                 (\cell ->
-                    cell
-                    |>  String.fromInt
+                    let
+                        display = case cell of
+                            EmptyCell ->
+                                ""
+
+                            StaticCell current ->
+                                String.fromInt current
+
+                            DynamicCell { merged } ->
+                                String.fromInt merged
+
+                    in
+                    display
                     |>  Element.text 
                     |>  Element.el
                         [   Element.width Element.fill
                         ,   Font.center
-                        -- ,   Element.height Element.fill
                         ,   Element.centerX
                         ,   Element.centerY
                         ]
